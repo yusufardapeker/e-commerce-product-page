99ad8e36bd08fc1f43ee54d1b6031ea87d66d043
Yusuf Arda Peker <yusufardapkr@gmail.com>
2024-06-22 15:52:36 +0300

    Code mobile menu

--- src/components/Header/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/Header/HamburgerMenu/HamburgerMenu.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import menuIcon from "../../../images/icon-menu.svg";
 
+import styles from "./styles.module.css";
+
+import closeIcon from "../../../images/icon-close.svg";
+
 function HamburgerMenu() {
-	return <img src={menuIcon} />;
+	const [show, setShow] = useState(false);
+
+	const showMobileNav = () => {
+		setShow(true);
+	};
+
+	const hideMobileNav = () => {
+		setShow(false);
+	};
+
+	return (
+		<>
+			<img src={menuIcon} onClick={showMobileNav} />
+			{show && (
+				<div className={styles.hamburgerMenu}>
+					<div className={styles.mobileNav}>
+						<img src={closeIcon} onClick={hideMobileNav} className={styles.closeIcon} />
+
+						<ul className={styles.navLinks}>
+							<li className={styles.link}>Collections</li>
+							<li className={styles.link}>Men</li>
+							<li className={styles.link}>Women</li>
+							<li className={styles.link}>About</li>
+							<li className={styles.link}>Contact</li>
+						</ul>
+					</div>
+					<div className={styles.overlay}></div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default HamburgerMenu;
--- src/components/Main/Showcase/ShowcaseMobile/index.jsx
+++ src/components/Main/Showcase/ShowcaseMobile/index.jsx
@@ -7,16 +7,40 @@ import nextIcon from "../../../../images/icon-next.svg";
 import prevIcon from "../../../../images/icon-previous.svg";
 
 function ShowcaseMobile() {
+	const wrapperElement = useRef();
+
+	let multiply = 0;
+
+	const changeImage = () => {
+		if (multiply === Images.length) {
+			multiply = 0;
+		} else if (multiply < 0) {
+			multiply = 3;
+		}
+
+		wrapperElement.current.style.transform = `translateX(${-375 * multiply}px)`;
+	};
+
+	const handlePrev = () => {
+		multiply--;
+		changeImage();
+	};
+
+	const handleNext = () => {
+		multiply++;
+		changeImage();
+	};
+
 	return (
 		<>
-			<div className={styles.showcaseWrapper}>
+			<div className={styles.showcaseWrapper} ref={wrapperElement}>
 				{Images.map((image, index) => {
 					return <img src={image} key={index} />;
 				})}
 			</div>
 
-			<img src={prevIcon} className={styles.prevIcon} />
-			<img src={nextIcon} className={styles.nextIcon} />
+			<img src={prevIcon} className={styles.prevIcon} onClick={handlePrev} />
+			<img src={nextIcon} className={styles.nextIcon} onClick={handleNext} />
 		</>
 	);
 }
